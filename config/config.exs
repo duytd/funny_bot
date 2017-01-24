3daa74dcdccffef2778d67faa9a00471b6fa6223
Duy Trinh Duc <duy.trinhduc@pixta.co.jp>
2017-01-24 11:12:14 +0700

    Add available locales

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :settings,
+  available_locales: [:en, :fr, :de, :nl, :es, :ko, :ja, :vi]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
